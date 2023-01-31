65f5b10b36c4dcc601a7141c821e2660615caba3
Yusuf-Sharif <93823265+Yusuf-Sharif@users.noreply.github.com>
2023-01-31 15:53:28 +0000

    Moving code to Github initially

---
+++ index.js
@@ -0,0 +1,59 @@
+
+import {dogs} from '/data.js'
+import Dog from '/Dog.js'
+// import {render} from '/utils.js'
+
+
+
+const divCrossBtn = document.getElementById("div-cross-btn");
+const divHeartBtn = document.getElementById("div-heart-btn");
+
+const imgNopeMsg = document.getElementById("img-nope-msg");
+const imgLikeMsg = document.getElementById("img-like-msg");
+
+const section2 = document.getElementsByClassName("section-2")[0]
+
+
+let isWaiting = false;
+
+
+const displayMsg = msgId => {
+  document.getElementById(msgId).style.display = "block";
+ 
+}
+
+
+divCrossBtn.addEventListener("click", function() {
+  displayMsg("img-nope-msg");
+
+  setTimeout( () => {
+    render();
+  }, 2000)
+  
+
+})
+
+divHeartBtn.addEventListener("click", () => {
+  displayMsg("img-like-msg");
+
+  setTimeout( () => {
+    render();
+  }, 2000)
+
+})
+
+
+
+
+const Rex = new Dog(dogs[0]);
+const Bella = new Dog(dogs[1]);
+const Teddy = new Dog(dogs[2]);
+
+
+
+
+export {section2, Rex, Bella, Teddy}
+
+section2.innerHTML = Rex.getProfileHtml();
+
+console.log(Rex.getProfileHtml())
